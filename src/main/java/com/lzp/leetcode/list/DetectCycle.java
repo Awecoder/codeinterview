aa567f6dcdad28de3768f9f74188427fc289e6e2
lizhongping <1607961042@qq.com>
2019-08-13 22:42:48 +0800

    环形链表2

---
+++ src/main/java/com/lzp/leetcode/list/DetectCycle.java
@@ -0,0 +1,27 @@
+package com.lzp.leetcode.list;
+
+/**
+ * 142. 环形链表 II
+ */
+public class DetectCycle {
+    /**
+     * Set去重法
+     *
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return head;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return null;
+    }
+}
